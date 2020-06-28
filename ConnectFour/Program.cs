b90b61146e256d706021bb2c8545821088444e9d
Paul Thurmond <paulthurmond@gmail.com>
2020-06-28 10:09:34 -0500

    Add project files.

---
+++ ConnectFour/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int[,] board = new int[7, 6];
+
+            board[3, 0] = 1;
+            board[2, 0] = 2;
+
+            PrintBoard(board);
+/*
+┌───┬───┬───┬───┬───┬───┬───┐
+│   │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┼───┤
+│   │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┼───┤
+│   │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┼───┤
+│   │   │   │   │   │   │   │
+├───┼───┼───┼───┼───┼───┼───┤
+│   │   │   │ O │   │   │   │
+├───┼───┼───┼───┼───┼───┼───┤
+│   │   │   │ X │   │   │   │
+└───┴───┴───┴───┴───┴───┴───┘
+*/
+
+        }
+
+        static void PrintBoard(int[,] boardData)
+        {
+            Console.Clear();
+            Console.WriteLine("\r\n           Connect Four\r\n");
+
+            Console.WriteLine("     1   2   3   4   5   6   7");
+            Console.WriteLine("   ┌───┬───┬───┬───┬───┬───┬───┐");
+            
+            for (int i = 5; i >= 0; i--)
+            {
+                Console.Write("   │ ");
+                for (int j = 0; j <= 6; j++)
+                {
+                    switch(boardData[j, i])
+                    {
+                        case 0:
+                            Console.Write("  │ ");
+                            break;
+                        case 1:
+                            Console.Write("X │ ");
+                            break;
+                        case 2:
+                            Console.Write("O │ ");
+                            break;
+                    }
+                }
+                Console.WriteLine("");
+
+                if (i > 0)
+                {
+                    Console.WriteLine("   ├───┼───┼───┼───┼───┼───┼───┤");
+                }
+            }
+            Console.WriteLine("   └───┴───┴───┴───┴───┴───┴───┘");
+        }
+    }
+}
